e3653b79e513814f4c47247ad5db87a9873a83d6
Bayan2019 <saparbayevabt@gmail.com>
2026-05-18 08:21:12 +0500

    Ch 3. Structured Logging Lv 5. Key-Value Pairs

--- server.go
+++ server.go
@@ -111,7 +111,14 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			next.ServeHTTP(w, r)
-			logger.Info(fmt.Sprintf("Served request: %s %s", r.Method, r.URL.Path))
+			// logger.Info(fmt.Sprintf("Served request: %s %s", r.Method, r.URL.Path))
+			// Ch 3. Structured Logging Lv 5. Key-Value Pairs
+			logger.Info(
+				"Served request",
+				"method", r.Method,
+				"path", r.URL.Path,
+				"client_ip", r.RemoteAddr,
+			)
 		})
 	}
 }
